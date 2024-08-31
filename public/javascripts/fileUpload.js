24a6bc791f4f555fa1d831d90e8c2714310671c4
Joseph Arambula <arambulajoseph@gmail.com>
2024-08-31 14:32:22 -0700

    finish project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,11 +1,45 @@
-// FilePond.registerPlugin(
-//     FilePondPluginImagePreview,
-//     FilePondPluginImageResize,
-//     FilePondPluginImageEncode,
+const rootStyles = window.getComputedStyle(document.documentElement)
 
-// )
 
-// FilePond.parse(document.body);
+if (rootStyles.getPropertyValue('--book-cover-width-large') != null && rootStyles.getPropertyValue('--book-cover-width-large') != '') {
+    ready()
+} else {
+    document.getElementById('main-css').addEventListener('load', ready)
+}
+
+function ready() {
+
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+    const coverHeight = coverWidth / coverAspectRatio
+
+    document.addEventListener('DOMContentLoaded', function() {
+        FilePond.registerPlugin(FilePondPluginImagePreview);
+        FilePond.registerPlugin(FilePondPluginImageResize);
+        FilePond.registerPlugin(FilePondPluginFileEncode);
+        const inputElement = document.querySelector('input[type="file"]');
+        const pond = FilePond.create(inputElement);
+    
+        pond.setOptions({
+            allowImageResize: true,
+            imagePreviewMaxHeight: 100,
+            stylePanelAspectRatio: 1 / coverAspectRatio,
+            imageResizeTargetWidth: coverWidth,
+            imageResizeTargetHeight: coverHeight,
+            imageResizeMode: 'contain',
+            imageResizeUpsacel: false
+        })
+    
+    
+        // pond.setOptions({
+        //     stylePanelAspectRatio: 150 / 100
+        // })
+        
+        FilePond.parse(document.body);
+      }); 
+
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
     FilePond.registerPlugin(FilePondPluginImagePreview);
@@ -18,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function() {
         allowImageResize: true,
         imagePreviewMaxHeight: 100,
         stylePanelAspectRatio: 150 / 100,
-        imageResizeTargetWidth: 50,
+        imageResizeTargetWidth: 200,
         imageResizeTargetHeight: 75,
         imageResizeMode: 'contain',
         imageResizeUpsacel: false
